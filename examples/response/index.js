123ab04380becfed035d2a7f1f090a628bcd7caf
Leonardo Oliva <fenix_de_plata@hotmail.com>
2018-04-09 17:16:27 -0500

    Response

--- examples/response/index.js
+++ examples/response/index.js
@@ -11,12 +11,37 @@ console.log('------------------------');
 console.log(res.format(
     { //Información relativa a la petición
 
-    },  
+    },
     200, // HTTP estatus
     { //Informacón principal
         name: 'will',
         lastaname: 'smith'
     },
+    { //Información Extra
+        extra: 'info'
+    }
+));
+console.log('------------------------');
+console.log('------------------------');
+console.log(res.format(
+    { //Información relativa a la petición
+
+    },
+    200, // HTTP estatus
+    [
+        { //Informacón principal
+            name: 'will',
+            lastaname: 'smith'
+        },
+        { //Informacón principal
+            name: 'will',
+            lastaname: 'smith'
+        },
+        { //Informacón principal
+            name: 'will',
+            lastaname: 'smith'
+        }
+    ],
     { //Información Extra
         extra: 'info'
     }
@@ -32,6 +57,32 @@ console.log(res.format(
         name: 'will',
         lastaname: 'smith'
     },
+    { //Información Extra
+        extra: 'info'
+    }
+));
+console.log('------------------------');
+console.log('------------------------');
+console.log(res.format(
+    { //Información relativa a la petición
+        method: 'post',
+        apiVersion: '1.0.2'
+    },
+    404, //HTTP Estatus
+    [
+        { //Informacón principal
+            name: 'will',
+            lastaname: 'smith'
+        },
+        { //Informacón principal
+            name: 'will 2',
+            lastaname: 'smith 2'
+        },
+        { //Informacón principal
+            name: 'will 3',
+            lastaname: 'smith 3'
+        }
+    ],
     { //Información Extra
         extra: 'info'
     }
